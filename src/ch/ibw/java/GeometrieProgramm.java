5d7d28cb79d2bb9fd3727dc226396e69981039a5
ptuor <tuor_philipp@hotmail.com>
2019-09-27 14:29:13 +0200

    Kommentar hinzugefügt

--- src/ch/ibw/java/GeometrieProgramm.java
+++ src/ch/ibw/java/GeometrieProgramm.java
@@ -3,7 +3,7 @@ package ch.ibw.java;
 public class GeometrieProgramm {
     public static void main(String[] args) {
 
-
+        //neues Kommentar
         Circle circle1 = Circle.create(6);
 
         System.out.println(circle1.calcSurface());
