f35cf8152ab2738c725952145e615b7f8462bf85
vinkja <spescha_marc@yahoo.de>
2019-09-27 14:06:47 +0200

    added some Files

---
+++ src/ch/ibw/java/PersonBeispiel.java
@@ -0,0 +1,11 @@
+package ch.ibw.java;
+
+public class PersonBeispiel {
+    String vorname;
+    String nachname;
+    int alter;
+
+    public String toString() {
+        return this.vorname + " " + this.nachname + ": " + this.alter;
+    }
+}
