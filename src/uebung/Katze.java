e35be03cf4942e4064c7a82ee93a05692eedd764
Gion <gion.desax@script-source.net>
2021-09-24 14:28:03 +0200

    Revert "Update Katze Getter/Setter"
    This reverts commit f006aaa1a9f41db70952fac9293602c9b5ae12ff.
    
    # Conflicts:
    #	src/uebung/Katze.java

--- src/uebung/Katze.java
+++ src/uebung/Katze.java
@@ -2,7 +2,48 @@ package uebung;
 
 public class Katze {
     private static String farbe;
-    public Katze(){
-        
+    private static String name;
+    private int groesse;
+    private int gewicht;
+
+    public Katze(String farbe, String name, int groesse, int gewicht){
+        this.farbe = farbe;
+        this.name = name;
+        this.groesse = groesse;
+        this.gewicht = gewicht;
+    }
+
+    public static String getFarbe() {
+        return farbe;
+    }
+
+    public static void setFarbe(String farbe) {
+        Katze.farbe = farbe;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        Katze.name = name;
+    }
+
+    public int getGroesse() {
+        return groesse;
+    }
+
+    public Katze setGroesse(int groesse) {
+        this.groesse = groesse;
+        return this;
+    }
+
+    public int getGewicht() {
+        return gewicht;
+    }
+
+    public Katze setGewicht(int gewicht) {
+        this.gewicht = gewicht;
+        return this;
     }
 }
