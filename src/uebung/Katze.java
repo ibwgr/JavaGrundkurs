8975b3330cc4df0ab052a48e209882aebe5c99e1
Gion <gion.desax@script-source.net>
2021-09-24 14:24:53 +0200

    Revert "Update Katze.java"
    This reverts commit 048665702f684b1f1918b17cbbe1a1d4fbbf1aae.
    
    # Conflicts:
    #	src/uebung/Katze.java

--- src/uebung/Katze.java
+++ src/uebung/Katze.java
@@ -3,47 +3,10 @@ package uebung;
 public class Katze {
     private static String farbe;
     private static String name;
-    private int groesse;
-    private int gewicht;
+    private static int groesse;
+    private static int gewicht;
+    public Katze(){
 
-    public Katze(String farbe, String name, int groesse, int gewicht){
-        this.farbe = farbe;
-        this.name = name;
-        this.groesse = groesse;
-        this.gewicht = gewicht;
-    }
-
-    public static String getFarbe() {
-        return farbe;
-    }
-
-    public static void setFarbe(String farbe) {
-        Katze.farbe = farbe;
-    }
-
-    public static String getName() {
-        return name;
-    }
-
-    public static void setName(String name) {
-        Katze.name = name;
-    }
-
-    public int getGroesse() {
-        return groesse;
-    }
-
-    public Katze setGroesse(int groesse) {
-        this.groesse = groesse;
-        return this;
-    }
-
-    public int getGewicht() {
-        return gewicht;
-    }
-
-    public Katze setGewicht(int gewicht) {
-        this.gewicht = gewicht;
-        return this;
+        
     }
 }
