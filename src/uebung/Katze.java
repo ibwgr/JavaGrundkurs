db51aa24f3c7a8330ab7a13f831afb3f9f4fbe2a
Gion <gion.desax@script-source.net>
2021-09-24 14:26:48 +0200

    Revert "Update Katze.java"
    This reverts commit 048665702f684b1f1918b17cbbe1a1d4fbbf1aae.

--- src/uebung/Katze.java
+++ src/uebung/Katze.java
@@ -2,11 +2,7 @@ package uebung;
 
 public class Katze {
     private static String farbe;
-    private static String name;
-    private static int groesse;
-    private static int gewicht;
     public Katze(){
-
         
     }
 }
