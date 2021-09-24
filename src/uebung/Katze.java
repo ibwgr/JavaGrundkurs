048665702f684b1f1918b17cbbe1a1d4fbbf1aae
Gion <gion.desax@script-source.net>
2021-09-24 13:51:28 +0200

    Update Katze.java

--- src/uebung/Katze.java
+++ src/uebung/Katze.java
@@ -2,7 +2,11 @@ package uebung;
 
 public class Katze {
     private static String farbe;
+    private static String name;
+    private static int groesse;
+    private static int gewicht;
     public Katze(){
+
         
     }
 }
