f5d6b03605b9f88386d9dd59e9e5aca39eaa1f0e
Gion <gion.desax@script-source.net>
2021-09-24 14:29:40 +0200

    Message2

--- src/uebung/Katze.java
+++ src/uebung/Katze.java
@@ -3,10 +3,47 @@ package uebung;
 public class Katze {
     private static String farbe;
     private static String name;
-    private static int groesse;
-    private static int gewicht;
-    public Katze(){
+    private int groesse;
+    private int gewicht;
 
-        
+    public Katze(String farbe, String name, int groesse, int gewicht){
+        this.farbe = farbe;
+        this.name = name;
+        this.groesse = groesse;
+        this.gewicht = gewicht;
+    }
+
+    public static String getFarbe() {
+        return farbe;
+    }
+
+    public static void setFarbe(String farbe) {
+        Katze.farbe = farbe;
+    }
+
+    public static String getName() {
+        return name;
+    }
+
+    public static void setName(String name) {
+        Katze.name = name;
+    }
+
+    public int getGroesse() {
+        return groesse;
+    }
+
+    public Katze setGroesse(int groesse) {
+        this.groesse = groesse;
+        return this;
+    }
+
+    public int getGewicht() {
+        return gewicht;
+    }
+
+    public Katze setGewicht(int gewicht) {
+        this.gewicht = gewicht;
+        return this;
     }
 }
